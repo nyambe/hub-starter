6519bdf42717244807ceedd8c1332b1052e3b7a0
Samuel Ebuka <samuel.ebuka@gmail.com>
2024-09-21 18:27:27 +0200

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
+  runtimeConfig: {
+    pineconeApiKey: '',
+  },
   // Nuxt 4 directory structure and features
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
---
+++ server/api/vector/index.ts
@@ -0,0 +1,4 @@
+// api/vector/index.ts
+export default defineEventHandler(async (event) => {
+  return 'Hello Nitro Vector'
+})
