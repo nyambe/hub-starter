91dc371da724a8d70611a2ccaffb974ca3380078
Samuel Ebuka <samuel.ebuka@gmail.com>
2024-09-23 13:11:32 +0200

    update envs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
   runtimeConfig: {
-    pineconeApiKey: '',
-    openaiApiKey: '',
-    anthropicApiKey: '',
-    qdrantApiKey: '',
+    pineconeApiKey: process.env.NUXT_PINECONE_API_KEY,
+    openaiApiKey: process.env.NUXT_OPENAI_API_KEY,
+    anthropicApiKey: process.env.NUXT_ANTHROPIC_API_KEY,
+    qdrantApiKey: process.env.NUXT_QDRANT_API_KEY,
     envTest: '0123',
-    envTest2: '0000',
+    envTest2: process.env.NUXT_ENV_TEST2,
   },
   // Nuxt 4 directory structure and features
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
