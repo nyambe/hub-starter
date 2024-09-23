4fe4fad570f300a0428e59ff429a08e884128152
Samuel Ebuka <samuel.ebuka@gmail.com>
2024-09-23 12:23:20 +0200

    embed c in string

--- server/api/vector/embed.ts
+++ server/api/vector/embed.ts
@@ -4,6 +4,9 @@ import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 
 export default defineEventHandler(async (event) => {
   // const embeddings = new OpenAIEmbeddings()
+  const query = getQuery(event)
+  let conversation = query.c as string || " En un lugar de la mancha"
+
   const config = useRuntimeConfig()
   const model = new OpenAIEmbeddings({
     apiKey: config.openaiApiKey,
@@ -12,7 +15,7 @@ export default defineEventHandler(async (event) => {
   //   new SystemMessage('You are a helpful assistant.'),
   //   new HumanMessage('I love programming.'),
   // ]
-  const conversation = "En un lugar de la mancha"
+
   const response = await model.embedQuery(conversation)
   return response
 })
